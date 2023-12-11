54f9a021de2084ec72e334a5f3ae790e57b7c114
Douglas <stokeioficial@gmail.com>
2023-12-11 19:50:26 -0300

    Change navbar

--- apps/website/views/app/course/pages/add-video/components/navbar/index.tsx
+++ apps/website/views/app/course/pages/add-video/components/navbar/index.tsx
@@ -1,23 +1,13 @@
 import { NavbarUserInformation, SidebarOpenButton } from "@/components";
-import { useTranslations } from "@/hooks";
-import { Navbar as NavbarUI, Title } from "@stokei/ui";
+import { Navbar as NavbarUI, Spacer } from "@stokei/ui";
 import { FC } from "react";
 
 interface NavbarProps {}
 
-export const Navbar: FC<NavbarProps> = () => {
-  const translate = useTranslations();
-  return (
-    <NavbarUI
-      align="center"
-      background="background.50"
-      borderBottomWidth="thin"
-    >
-      <SidebarOpenButton />
-      <Title fontSize="md" lineHeight="shorter">
-        {translate.formatMessage({ id: "addVideo" })}
-      </Title>
-      <NavbarUserInformation />
-    </NavbarUI>
-  );
-};
+export const Navbar: FC<NavbarProps> = () => (
+  <NavbarUI align="center" background="background.50" borderBottomWidth="thin">
+    <SidebarOpenButton />
+    <Spacer />
+    <NavbarUserInformation />
+  </NavbarUI>
+);
