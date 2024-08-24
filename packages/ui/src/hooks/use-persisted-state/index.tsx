f7a05acc4c98aef113eff0595ca78eaa6e45ead5
Douglas <stokeioficial@gmail.com>
2024-08-23 21:31:59 -0300

    Add local storage

--- packages/ui/src/hooks/use-persisted-state/index.tsx
+++ packages/ui/src/hooks/use-persisted-state/index.tsx
@@ -1,43 +1,49 @@
-import cookies from "js-cookie";
 import { useCallback, useEffect, useState } from "react";
 
 const initializeState = (key: string) => {
-  const currentValue = cookies.get(key);
-  if (!currentValue) {
-    return;
-  }
-  try {
-    return JSON.parse(currentValue);
-  } catch (e) {
-    return;
-  }
+	if (typeof window === 'undefined') {
+		return;
+	}
+	const currentValue = window.localStorage.getItem(key);
+	if (!currentValue) {
+		return;
+	}
+	try {
+		return JSON.parse(currentValue);
+	} catch (e) {
+		return;
+	}
 };
 
 export function usePersistedState<TValue = any>({ key }: { key: string }) {
-  const [value, setValue] = useState<TValue>();
+	const [value, setValue] = useState<TValue>();
 
-  useEffect(() => {
-    setValue(initializeState(key));
-  }, [key]);
+	useEffect(() => {
+		setValue(initializeState(key));
+	}, [key]);
 
-  const onSetValue = useCallback(
-    (currentValue: (prevState: TValue | undefined) => TValue | undefined) => {
-      setValue((prevState) => {
-        if (typeof currentValue === "function") {
-          const newValue = currentValue(prevState);
-          if (newValue) {
-            cookies.set(key, JSON.stringify(newValue));
-            return newValue;
-          }
-        } else {
-          cookies.set(key, JSON.stringify(currentValue));
-          return currentValue;
-        }
-        return prevState;
-      });
-    },
-    [key]
-  );
+	const onSetValue = useCallback(
+		(currentValue: (prevState: TValue | undefined) => TValue | undefined) => {
+			setValue((prevState) => {
+				try {
+					if (typeof currentValue === "function") {
+						const newValue = currentValue(prevState);
+						if (newValue) {
+							window.localStorage.setItem(key, JSON.stringify(newValue));
+							return newValue;
+						}
+					} else {
+						window.localStorage.setItem(key, JSON.stringify(currentValue));
+						return currentValue;
+					}
+				} catch (error) {
+					return prevState;
+				}
+				return prevState;
+			});
+		},
+		[key]
+	);
 
-  return { value, setValue: onSetValue };
+	return { value, setValue: onSetValue };
 }
